22978bf875402a78984e1ab8f8eb9172f2867188
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-11-12 16:00:00 -0500

    Add Request Status (#5)
    * Add Docker support with Dockerfile and .dockerignore; run app on main thread
    
    * Set working directory, add health check, and create non-root user
    
    * Add .DS_Store to .gitignore to prevent tracking of macOS system files
    
    * Add success status to JSON responses
    
    * Update JSON response format to include success status

--- app.py
+++ app.py
@@ -22,5 +22,14 @@ def server_time():
     return jsonify(server_time=time.strftime("%H:%M:%S %d/%m/%Y"))
 
 
+@app.after_request
+def add_status(response):
+    if response.is_json:
+        original_data = response.get_json()
+        original_data["success"] = response.status_code == 200
+        response.set_data(jsonify(original_data).data)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
