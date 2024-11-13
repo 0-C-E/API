c115e614f579aca0f93d3183f85bb068fbd6b4c2
Vianpyro <vianney@veremme.org>
2024-11-12 22:18:34 -0500

    Add route to retrieve all worlds from the database

--- routes/world.py
+++ routes/world.py
@@ -2,3 +2,14 @@ from flask import Blueprint, jsonify, request
 from db import get_db_connection
 
 worlds_blueprint = Blueprint("worlds", __name__)
+
+
+@worlds_blueprint.route("", methods=["GET"])
+def get_all_worlds():
+    db = get_db_connection()
+    with db.cursor() as cursor:
+        cursor.callproc("get_all_worlds")
+        worlds = cursor.fetchall()
+    db.close()
+
+    return jsonify(worlds)
