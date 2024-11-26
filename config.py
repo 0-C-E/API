b7f9ce62aaca50d2e87ccf2a4bc08a6458349e4c
Vianpyro <vianney@veremme.org>
2024-11-26 15:09:36 -0500

    Implement rate limiting with Flask-Limiter and add error handling for rate limit exceeded

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 
 from dotenv import load_dotenv
-
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 # Load environment variables from .env file
 load_dotenv()
@@ -13,3 +14,9 @@ class Config:
     MYSQL_PASSWORD = os.getenv("DB_PASSWORD", "myrootpassword")
     MYSQL_DB = os.getenv("DB_NAME", "0ce")
     MYSQL_CURSORCLASS = "DictCursor"
+
+
+limiter = Limiter(
+    key_func=get_remote_address,
+    default_limits=["1000 per day", "200 per hour", "30 per minute", "3 per second"],
+)
